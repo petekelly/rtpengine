e8f7297601e431eb1da8e95ac79c66aa6152a185
Peter Kelly <pkelly@gmail.com>
2025-05-16 16:13:15 +0100

    fix for strange reused ssrc issue

--- recording-daemon/mix.h
+++ recording-daemon/mix.h
@@ -3,14 +3,15 @@
 
 #include "types.h"
 #include <libavutil/frame.h>
+#include <stdint.h>
 
-#define MIX_MAX_INPUTS 4
+#define MIX_MAX_INPUTS 8
 
 mix_t *mix_new(void);
 void mix_destroy(mix_t *mix);
 void mix_set_channel_slots(mix_t *mix, unsigned int);
 int mix_config(mix_t *, const format_t *format);
-int mix_add(mix_t *mix, AVFrame *frame, unsigned int idx, void *, output_t *output);
-unsigned int mix_get_index(mix_t *, void *, unsigned int, unsigned int);
+int mix_add(mix_t *mix, AVFrame *frame, unsigned int idx, uint32_t ssrc, output_t *output);
+unsigned int mix_get_index(mix_t *, uint32_t ssrc, unsigned int, unsigned int);
 #endif
 
